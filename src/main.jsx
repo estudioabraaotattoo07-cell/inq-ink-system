eeac35132c98386e96b95e06c86b30b199fb7c52
estudioabraaotattoo07-cell <estudioabraaotattoo07@gmail.com>
2026-05-31 09:21:17 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CRM from './CRM_Casa_dos_Carvalho'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CRM />
+  </React.StrictMode>
+)
